6f04c96c95cbc43a0e1b2157aea3e7ee7bfe06a6
yaozou <yaozou@pand-auto.com>
2019-07-30 17:16:01 +0800

    熔断器

--- inner-api/src/main/java/com/yao/service/proxy/api/uer/IUserProvider.java
+++ inner-api/src/main/java/com/yao/service/proxy/api/uer/IUserProvider.java
@@ -3,12 +3,14 @@ package com.yao.service.proxy.api.uer;
 import com.yao.service.proxy.api.IProxyProvider;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
+import static com.yao.service.proxy.api.IProxyProvider.USER_FEFIN;
+
 /**
  * @Description:
  * @Auther: yaozou
  * @Date: 2018/8/22 14:38
  */
-@FeignClient
+@FeignClient(name = USER_FEFIN,fallback = UserFeignFallback.class)
 public interface IUserProvider extends IProxyProvider{
 
 }
---
+++ inner-api/src/main/java/com/yao/service/proxy/api/uer/UserFeignFallback.java
@@ -0,0 +1,10 @@
+package com.yao.service.proxy.api.uer;
+
+/**
+ * @Description:
+ * @author: yaozou
+ * @Date: 2019/7/30 17:14
+ */
+public class UserFeignFallback implements IUserProvider  {
+
+}
